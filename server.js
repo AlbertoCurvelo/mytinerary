ebc122aa105a967120caec22430cce54bd51623d
BuildTools <unconfigured@null.spigotmc.org>
2021-02-26 00:22:11 -0300

    Subida a Heroku - Backend

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 /*Importaciones*/
 const express = require('express')
+const path = require('path')
 require('dotenv').config()
+require('./config/database')
 const cors=require('cors')
 const router = require ('./routes/index')
-require('./config/database')
-
 const app = express()
+
 /*Middlewares*/
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 /*Pedidos*/
 app.use("/api",router)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname+"/client/build/index.html"))
+  })
+}
+const port = process.env.PORT || 4000
+const host = process.env.HOST || '0.0.0.0'
+
+app.listen(port, host,  console.log('app listening'))
 /*Server port e iniciacion*/
 app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}`))
